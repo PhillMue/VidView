5afaff7919abed1e4a4af60a1c5887d588d72000
PhillMue <muenzuve@gmail.com>
2016-04-13 11:58:32 +0300

    generated apk

--- app/src/main/java/org/schabi/vidview/ActionBarHandler.java
+++ app/src/main/java/org/schabi/vidview/ActionBarHandler.java
@@ -16,25 +16,7 @@ import org.schabi.vidview.extractor.VideoStream;
 
 import java.util.List;
 
-/**
- * Created by Christian Schabesberger on 18.08.15.
- *
- * Copyright (C) Christian Schabesberger 2015 <chris.schabesberger@mailbox.org>
- * DetailsMenuHandler.java is part of NewPipe.
- *
- * NewPipe is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * NewPipe is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 
 class ActionBarHandler {
